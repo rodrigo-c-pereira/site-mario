42836a9ac6f1dc7ab83d3ba7b8089e31e3c80d29
Rodrigo Pereira <rodrigocostarcp@outlook.com>
2023-07-17 17:15:45 +0100

    projeto de site do Mario Bros feito com ajuda do Dev Em Dobro

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+console.log("mostrar o document");
+
+
+const botaoTrailer = document.querySelector('.botao-trailer');
+const modal = document.querySelector(".modal");
+const video = document.getElementById("video");
+const linkDoVideo = video.src;
+
+
+botaoTrailer.addEventListener("click", () => {
+    modal.classList.add("aberto"); 
+    video.setAttribute("src", linkDoVideo);
+});
+
+
+
+const botaoFecharModal = document.querySelector(".fechar-modal");
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto");
+    video.setAttribute("src", "");
+});
+
+
